f1432486fe7bf6898648e7ee86312f9b570ec3ca
Khardoin <87874497+Khardoin@users.noreply.github.com>
2021-07-23 14:13:44 -0400

    Add files via upload

---
+++ Challenge 1/challenge 1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Challenge_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Tavern Interaction";
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Barkeeper - Hello stranger, what can I do for you?");
+
+            Console.ForegroundColor = ConsoleColor.Green; 
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("You don't say? well, I think that can be arranged!");
+
+            Console.ForegroundColor = ConsoleColor.Green; 
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Thug - I don't like the cutuva jib boy, get lost, we don't need weaklings around these parts");
+
+            Console.ForegroundColor = ConsoleColor.Green; 
+            Console.ReadLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
